69ce6ed3016354e5b0ef1767e315c9727d3258a1
Arjun Ajith <arjun.ajith@hpe.com>
2021-03-15 17:55:41 +0530

    added connector interface for DB

---
+++ capmodel/common.go
@@ -0,0 +1,42 @@
+//(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+//
+//Licensed under the Apache License, Version 2.0 (the "License"); you may
+//not use this file except in compliance with the License. You may obtain
+//a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//License for the specific language governing permissions and limitations
+// under the License.
+
+package capmodel
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/ODIM-Project/PluginCiscoACI/db"
+)
+
+const (
+	// TableFabric is the table for storing switch and pod ids
+	TableFabric = "ACIFabric"
+	// TableSwitch is the table for storing switch information
+	TableSwitch = "ACISwitch"
+	// TableSwitchPorts is the table for storing ports of each switch
+	TableSwitchPorts = "ACISwitchPorts"
+	// TablePort is the table for storing port information
+	TablePort = "ACIPort"
+)
+
+// SaveToDB is for adding port data to the DB
+func SaveToDB(table, resourceID string, data interface{}) error {
+	dataByte, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("while marshalling data, got: %v", err)
+	}
+	return db.Connector.Create(table, resourceID, string(dataByte))
+}
--- db/client_test.go
+++ db/client_test.go
@@ -43,9 +43,9 @@ func TestGetClient(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetClient()
+			got, err := getClient()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GetClient() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("getClient() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got.pool == nil {
